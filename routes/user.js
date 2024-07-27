ee3254d280e3ccca1f814f19a01c8cdf6efdc5fd
Soumyajit Saha <jitsaha567@gmail.com>
2024-07-27 10:52:37 +0530

    4th update

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ router.route("/signup")
 
 router.route("/login")
 .get( userController.loginPage)
-.post( saveRedirectUrl, passport.authenticate("local" , {failureRedirect: "/login" , failureFlash:true,}), userController.loginPageFlash);
+.post( saveRedirectUrl, passport.authenticate("local" , {failureRedirect: "/login" , failureFlash:true,}), );
 
 router.get("/logout" , userController.logout);
 module.exports = router;
